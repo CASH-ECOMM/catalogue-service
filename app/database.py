4c8792ff81d5d8af1030d907123d214b98c1daf8
david <david.zinkiv@gmail.com>
2025-10-26 17:19:12 -0400

    fixed db url

--- app/database.py
+++ app/database.py
@@ -1,8 +1,11 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import os
 
-DATABASE_URL = "postgresql://dave:@localhost:5432/catalogue_db"
+DATABASE_URL = os.getenv(
+    "DATABASE_URL", "postgresql://postgres:@localhost:5432/catalogue_db"
+)
 
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
